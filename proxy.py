cb24ae2c655f681e26692076e2084d02c8efc2b8
Josh Durbin <durbinjo593@gmail.com>
2017-03-24 16:49:10 -0700

    first pass

--- proxy.py
+++ proxy.py
@@ -3,16 +3,36 @@ from __future__ import print_function
 import json
 import urllib2
 import ssl
+import os
 
-import time
-import datetime
+target_server = os.environ['webserver_internal_ip']
 
 def lambda_handler(event, context):
 
-    #return "time= " + str(time.time()) + " | new time= " + str((datetime.datetime.utcnow() + datetime.timedelta(days=7)))
+    print("Got event\n" + json.dumps(event, indent=2))
+
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+
+    req = urllib2.Request(target_server + event['path'])
+
+    if event['body']:
+        req.add_data(event['body'])
+
+    for header in event['headers']:
+        req.add_header(header, event['headers'][header])
+
     out = {}
 
-    out['statusCode'] = 200
-    out['body'] = ''
+    try:
+        resp = urllib2.urlopen(req, context=ctx)
+        out['statusCode'] = resp.getcode()
+        out['body'] = resp.read()
+
+    except urllib2.HTTPError as e:
+
+        out['statusCode'] = e.getcode()
+        out['body'] = e.read()
 
     return out
