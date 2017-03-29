6878ad0cfec13d62d83b22d92a08f8efe005afa4
Josh Durbin <durbinjo593@gmail.com>
2017-03-29 07:38:27 -0700

    Adding outputs

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "elb_endpoint" {
+  value = "${aws_elb.elb.dns_name}"
+}
+
+output "api_gateway_endpoint" {
+  value = "https://${aws_api_gateway_deployment.proxy_api_poc_deployment.rest_api_id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_deployment.proxy_api_poc_deployment.stage_name}"
+}
