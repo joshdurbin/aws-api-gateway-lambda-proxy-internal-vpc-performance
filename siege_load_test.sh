0a22e360068771d607343ecf6b5096f415fe6980
Josh Durbin <durbinjo593@gmail.com>
2017-04-05 13:58:56 -0700

    Added var

--- siege_load_test.sh
+++ siege_load_test.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
+ELB="http://public-elb-1171831284.us-west-2.elb.amazonaws.com"
+API_GW_LAMBDA="https://4z2sy32w8d.execute-api.us-west-2.amazonaws.com"
+API_GW_HTTP_PROXY="https://zefqgwbl3c.execute-api.us-west-2.amazonaws.com"
+
 echo "____________ Testing ELB root ____________________________________________"
-siege http://public-elb-1171831284.us-west-2.elb.amazonaws.com --benchmark --time=10S
+siege $ELB --benchmark --time=10S
 echo "____________ Testing ELB /child/random.txt _______________________________"
-siege http://public-elb-1171831284.us-west-2.elb.amazonaws.com/child/random.txt --benchmark --time=10S
+siege $ELB/child/random.txt --benchmark --time=10S
 echo "____________ Testing ELB /child/grandchild/random.txt ____________________"
-siege http://public-elb-1171831284.us-west-2.elb.amazonaws.com/child/grandchild/random.txt --benchmark --time=10S
+siege $ELB/child/grandchild/random.txt --benchmark --time=10S
 
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
 echo "____________ Testing API Gateway Lambda Proxy root _______________________"
-siege https://4z2sy32w8d.execute-api.us-west-2.amazonaws.com/test --benchmark --time=10S
+siege $API_GW_LAMBDA/test --benchmark --time=10S
 echo "____________ Testing API Gateway Lambda Proxy /child/random.txt __________"
-siege https://4z2sy32w8d.execute-api.us-west-2.amazonaws.com/test/child/random.txt --benchmark --time=10S
+siege $API_GW_LAMBDA/test/child/random.txt --benchmark --time=10S
 echo "____________ Testing API Gateway Lambda Proxy /child/grandchild/random.txt"
-siege https://4z2sy32w8d.execute-api.us-west-2.amazonaws.com/test/child/grandchild/random.txt --benchmark --time=10S
+siege $API_GW_LAMBDA/test/child/grandchild/random.txt --benchmark --time=10S
 
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
 echo "____________ Testing API Gateway HTTP Proxy root _________________________"
-siege https://zefqgwbl3c.execute-api.us-west-2.amazonaws.com/test  --benchmark --time=10S
+siege $API_GW_HTTP_PROXY/test  --benchmark --time=10S
 echo "____________ Testing API Gateway HTTP Proxy /child/random.txt ____________"
-siege https://zefqgwbl3c.execute-api.us-west-2.amazonaws.com/test/child/random.txt --benchmark --time=10S
+siege $API_GW_HTTP_PROXY/test/child/random.txt --benchmark --time=10S
 echo "____________ Testing API Gateway HTTP Proxy /child/grandchild/random.txt _"
-siege https://zefqgwbl3c.execute-api.us-west-2.amazonaws.com/test/child/grandchild/random.txt --benchmark --time=10S
+siege $API_GW_HTTP_PROXY/test/child/grandchild/random.txt --benchmark --time=10S
